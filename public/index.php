f752290ee54254d6e2c383d8b477807abf2f1bb9
syaob <syaob@yahoo.fr>
2024-12-04 09:55:07 +0100

    ajout workflow de deploiement maj version appleboy - index => demo du jour Mercredi bug

--- public/index.php
+++ public/index.php
@@ -1,3 +1,3 @@
 <?php
 header('Content-Type: application/json');
-echo json_encode(['message' => 'Hello CLOUD CAMPUS HATEM EN PLUS MAJ from local Docker environment!']);
+echo json_encode(['message' => 'Hello CLOUD CAMPUS HATEM EN PLUS MAJ from local Docker environment! bug site']);
